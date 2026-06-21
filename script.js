1c5293fdeb20b009824130d2976fffa7dfc375e1
The Rewon <mrevan6067@gmail.com>
2026-06-22 02:07:48 +0400

    initial state

--- script.js
+++ script.js
@@ -1,6 +1,222 @@
+const davamEdenLayiheler = [
+  {
+    name: "Kapital Bank",
+    image: "/uploads/posts/2024-08/1722944544_kapital-bnak.jpg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/116-kapitalbank.html"
+  },
+  {
+    name: "Astara international Server",
+    image: "/uploads/posts/2025-08/medium/1756489615_astara-dc.jpeg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/115-astara-international-server.html"
+  },
+  {
+    name: "Delta Telecom",
+    image: "/uploads/posts/2024-08/1722943570_delta-telecom.jpg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/113-delta-telecom-ana-server-odalar-gazl-sondurme-ve-hybrid-alglama-sistemleri.html"
+  },
+  {
+    name: "Şamaxı Pirqulu Otel",
+    image: "/uploads/posts/2024-08/1722943487_samaxi-palace-platinum.jpg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/112-amax-pirqulu-otel.html"
+  },
+  {
+    name: "Sabah Residence",
+    image: "/uploads/posts/2024-02/1707128672_sabah.jpeg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/62-sabah-residence.html"
+  },
+  {
+    name: "Şeredil Hotel",
+    image: "/uploads/posts/2024-02/1707128871_seredil.jpeg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/61-eredil-hotel.html"
+  },
+  {
+    name: "Hilton Garden INN",
+    image: "/uploads/posts/2024-02/1707129009_hilton.jpg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/60-hilton-garden-inn.html"
+  },
+  {
+    name: "Ağdam City Hotel",
+    image: "/uploads/posts/2024-02/1707129090_agdam.jpeg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/59-adam-city-hotel.html"
+  },
+  {
+    name: "Sarabski Plaza",
+    image: "/uploads/posts/2024-02/1706958384_sarabski-r3-1500x844.jpg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/58-sarabski-plaza.html"
+  },
+  {
+    name: "Delta Telecom New Data center",
+    image: "/uploads/posts/2024-02/1707129967_delta.jpeg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/57-delta-telecom-new-data-center.html"
+  },
+  {
+    name: "Sevinc Mall AVM",
+    image: "/uploads/posts/2024-02/1707130185_sevinc.jpeg",
+    url: "https://nareks.com/layiheler/davam-eden-layiheler/56-sevinc-mall-avm.html"
+  }
+];
+
+ const bitmisLayiheler = [
+  {
+    name: "Azərbaycan Prezident Administrasiyası",
+    image: "/uploads/posts/2024-08/1722942845_33e5xhncz9_000035.jpeg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/110-azrbaycan-prezident-administrasiyas.html"
+  },
+  {
+    name: "Heydar Aliyev Congress Hall",
+    image: "/uploads/posts/2024-08/1722942422_unnamed-1.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/109-heydar-aliyev-congress-hall.html"
+  },
+  {
+    name: "Excelsior Hotel & Spa",
+    image: "/uploads/posts/2024-08/1722942066_250857907.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/108-excelsior-hotel-amp-spa.html"
+  },
+  {
+    name: "Silkway Travel",
+    image: "/uploads/posts/2024-08/1722941977_unnamed.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/107-silkway-nizami-126-idare-ofisleri.html"
+  },
+  {
+    name: "İnternational Broadcast Center",
+    image: "/uploads/posts/2024-08/1722941483_fachada-ibc.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/106-nternational-broadcast-center.html"
+  },
+  {
+    name: "Azərbaycan Beynəlxalq Bankı",
+    image: "/uploads/posts/2024-08/1722941397_abb-bina-2020.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/105-azrbaycan-beynlxalq-bank.html"
+  },
+  {
+    name: "AGAKHAN MEDICAL CENTRE (8 Operation Theather / 50 Bed)",
+    image: "/uploads/posts/2024-08/1722941086_eg_5gt8xyaa160b-e1655159969672.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/104-agakhan-medical-centre-8-operation-theather-50-bed.html"
+  },
+  {
+    name: "Sanatoria Istochnik Hotel RUSSIA",
+    image: "/uploads/posts/2024-08/1722940972_sanatoriy-istochnik-general-cba0995.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/103-sanatoria-istochnik-hotel-russia.html"
+  },
+  {
+    name: "Holiday Inn Hotel Baku",
+    image: "/uploads/posts/2024-08/1722940955_holiday-inn-baku-3738671928-4x3.webp",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/102-holiday-inn-hotel-baku.html"
+  },
+  {
+    name: "Aquatic Swimming Pool",
+    image: "/uploads/posts/2024-08/1722940757_aquatic.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/101-aquatic-swimming-pool.html"
+  },
+  {
+    name: "Amburan Mall",
+    image: "/uploads/posts/2024-08/1722940465_amburan.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/100-amburan-mall.html"
+  },
+  {
+    name: "Gazelli Evi Targovi Spa & Fitness",
+    image: "/uploads/posts/2024-08/1722940176_gazelli.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/99-gazelli-evi-targovi-spa-amp-fitness.html"
+  },
+  {
+    name: "Goranboy Olimpiya İdman Kompleksi",
+    image: "/uploads/posts/2024-08/1722939871_goranboy.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/98-goranboy-olimpiya-dman-kompleksi.html"
+  },
+  {
+    name: "Xəzər Dəniz Gəmiçiliyi Esas binası",
+    image: "/uploads/posts/2024-08/1722939683_xezer.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/97-sabah-residence-premium-yaay-kompleksi-amp-hotel.html"
+  },
+  {
+    name: "Sabah Residence - Premium Yaşayış Kompleksi & Hotel",
+    image: "/uploads/posts/2024-08/1722939534_sabah-residence.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/96-sabah-residence-premium-yaay-kompleksi-amp-hotel.html"
+  },
+  {
+    name: "Intercontinental Hotel BAKU",
+    image: "/uploads/posts/2024-08/1722939442_intercont.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/95-intercontinental-hotel-baku.html"
+  },
+  {
+    name: "Knights Bridge",
+    image: "/uploads/posts/2024-08/1722939189_4569.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/94-knights-bridge.html"
+  },
+  {
+    name: "Yavuz Sultan Səlim körpüsü",
+    image: "/uploads/posts/2024-08/1722938994_yavuz-sultan-selim-koprusu-aa-1903882.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/93-yavuz-sultan-slim-korpusu.html"
+  },
+  {
+    name: "Delta Telecom",
+    image: "/uploads/posts/2024-08/1722938493_9787_delta4.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/92-delta-telecom.html"
+  },
+  {
+    name: "Ravy Tower",
+    image: "/uploads/posts/2024-08/1722934779_bakubayhorizontal-copy-min.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/91-ravy-tower.html"
+  },
+  {
+    name: "Alov Qüllələri Residence",
+    image: "/uploads/posts/2024-08/1722934390_alov-qulleleri.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/90-alov-qulllri-residence.html"
+  },
+  {
+    name: "Şahdağ Turizm Mərkəzi",
+    image: "/uploads/posts/2024-08/1722931129_sahdag.webp",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/89-ahda-turizm-mrkzi.html"
+  },
+  {
+    name: "Azərbaycan Respublikası Montin Data Center Ağa Nemetulla Bakı",
+    image: "/uploads/posts/2024-08/1722683369_istock-599903446-630x378.jpg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/85-azrbaycan-respublikas-montin-data-center-aa-nemetulla-bak.html"
+  },
+  {
+    name: "Socar Azerkimya IB SUMGAYIT",
+    image: "/uploads/posts/2024-08/1722674009_soc1.jpeg",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/79-socar-azerkimya-ib-sumgayit.html"
+  },
+  {
+    name: "Heydər Əliyev adına Neft Emalı Zavodu",
+    image: "/uploads/posts/2024-08/1722673752_image-1-heydar-refinery.webp",
+    url: "https://nareks.com/layiheler/bitmis-layiheler/77-heydr-liyev-adna-neft-emal-zavodu.html"
+  }
+];
 
 const bottomHeader = document.getElementById("bottomHeader");
+const headingProject = document.getElementById("heading-project")
+const navProject = document.getElementById("nav-project")
+const layiheler = document.querySelector(".projects__inner")
 
+const params = new URLSearchParams(window.location.search);
+const page = params.get("page");
+
+if(page === 'ungoing' ) {
+    headingProject.innerHTML = "Davam edən layihələr"
+    navProject.innerHTML = "Ana səhifə  &rarr; Davam edən layihələr"
+    layiheler.innerHTML = davamEdenLayiheler.map(item=> (
+        `
+            <div class="swiper-slide"> 
+                <span>${item.name}</span> 
+                <img src=${"https://nareks.com" + item.image}> 
+                <a href="https://nareks.com/layiheler/bitmis-layiheler/110-azrbaycan-prezident-administrasiyas.html" class="project-btn">Daha ətraflı</a> 
+            </div>
+        `
+    )).join("");
+}else if(page === 'completed') {    
+    headingProject.innerHTML = "Bitmiş layihələr"  
+    navProject.innerHTML = "Ana səhifə  &rarr; Bitmiş layihələr"
+    layiheler.innerHTML = bitmisLayiheler.map(item=> (
+        `
+            <div class="swiper-slide"> 
+                <span>${item.name}</span> 
+                <img src=${"https://nareks.com" + item.image}> 
+                <a href="https://nareks.com/layiheler/bitmis-layiheler/110-azrbaycan-prezident-administrasiyas.html" class="project-btn">Daha ətraflı</a> 
+            </div>
+        `
+    )).join("");  
+}
 
 
 window.addEventListener("scroll", () => {
