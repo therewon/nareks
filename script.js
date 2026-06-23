9c5c8fff22a4fa56c882565b2435f942707d45d0
therewon <mrevan666@gmail.com>
2026-06-23 14:11:53 +0400

    update

--- script.js
+++ script.js
@@ -1,183 +1,219 @@
 const davamEdenLayiheler = [
   {
+    category: "Davam edən layihələr",
     name: "Kapital Bank",
     image: "/uploads/posts/2024-08/1722944544_kapital-bnak.jpg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/116-kapitalbank.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Astara international Server",
     image: "/uploads/posts/2025-08/medium/1756489615_astara-dc.jpeg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/115-astara-international-server.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Delta Telecom",
     image: "/uploads/posts/2024-08/1722943570_delta-telecom.jpg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/113-delta-telecom-ana-server-odalar-gazl-sondurme-ve-hybrid-alglama-sistemleri.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Şamaxı Pirqulu Otel",
     image: "/uploads/posts/2024-08/1722943487_samaxi-palace-platinum.jpg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/112-amax-pirqulu-otel.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Sabah Residence",
     image: "/uploads/posts/2024-02/1707128672_sabah.jpeg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/62-sabah-residence.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Şeredil Hotel",
     image: "/uploads/posts/2024-02/1707128871_seredil.jpeg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/61-eredil-hotel.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Hilton Garden INN",
     image: "/uploads/posts/2024-02/1707129009_hilton.jpg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/60-hilton-garden-inn.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Ağdam City Hotel",
     image: "/uploads/posts/2024-02/1707129090_agdam.jpeg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/59-adam-city-hotel.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Sarabski Plaza",
     image: "/uploads/posts/2024-02/1706958384_sarabski-r3-1500x844.jpg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/58-sarabski-plaza.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Delta Telecom New Data center",
     image: "/uploads/posts/2024-02/1707129967_delta.jpeg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/57-delta-telecom-new-data-center.html"
   },
   {
+    category: "Davam edən layihələr",
     name: "Sevinc Mall AVM",
     image: "/uploads/posts/2024-02/1707130185_sevinc.jpeg",
     url: "https://nareks.com/layiheler/davam-eden-layiheler/56-sevinc-mall-avm.html"
   }
 ];
 
- const bitmisLayiheler = [
+const bitmisLayiheler = [
   {
+    category: "Bitmiş layihələr",
     name: "Azərbaycan Prezident Administrasiyası",
     image: "/uploads/posts/2024-08/1722942845_33e5xhncz9_000035.jpeg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/110-azrbaycan-prezident-administrasiyas.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Heydar Aliyev Congress Hall",
     image: "/uploads/posts/2024-08/1722942422_unnamed-1.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/109-heydar-aliyev-congress-hall.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Excelsior Hotel & Spa",
     image: "/uploads/posts/2024-08/1722942066_250857907.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/108-excelsior-hotel-amp-spa.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Silkway Travel",
     image: "/uploads/posts/2024-08/1722941977_unnamed.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/107-silkway-nizami-126-idare-ofisleri.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "İnternational Broadcast Center",
     image: "/uploads/posts/2024-08/1722941483_fachada-ibc.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/106-nternational-broadcast-center.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Azərbaycan Beynəlxalq Bankı",
     image: "/uploads/posts/2024-08/1722941397_abb-bina-2020.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/105-azrbaycan-beynlxalq-bank.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "AGAKHAN MEDICAL CENTRE (8 Operation Theather / 50 Bed)",
     image: "/uploads/posts/2024-08/1722941086_eg_5gt8xyaa160b-e1655159969672.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/104-agakhan-medical-centre-8-operation-theather-50-bed.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Sanatoria Istochnik Hotel RUSSIA",
     image: "/uploads/posts/2024-08/1722940972_sanatoriy-istochnik-general-cba0995.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/103-sanatoria-istochnik-hotel-russia.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Holiday Inn Hotel Baku",
     image: "/uploads/posts/2024-08/1722940955_holiday-inn-baku-3738671928-4x3.webp",
     url: "https://nareks.com/layiheler/bitmis-layiheler/102-holiday-inn-hotel-baku.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Aquatic Swimming Pool",
     image: "/uploads/posts/2024-08/1722940757_aquatic.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/101-aquatic-swimming-pool.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Amburan Mall",
     image: "/uploads/posts/2024-08/1722940465_amburan.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/100-amburan-mall.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Gazelli Evi Targovi Spa & Fitness",
     image: "/uploads/posts/2024-08/1722940176_gazelli.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/99-gazelli-evi-targovi-spa-amp-fitness.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Goranboy Olimpiya İdman Kompleksi",
     image: "/uploads/posts/2024-08/1722939871_goranboy.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/98-goranboy-olimpiya-dman-kompleksi.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Xəzər Dəniz Gəmiçiliyi Esas binası",
     image: "/uploads/posts/2024-08/1722939683_xezer.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/97-sabah-residence-premium-yaay-kompleksi-amp-hotel.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Sabah Residence - Premium Yaşayış Kompleksi & Hotel",
     image: "/uploads/posts/2024-08/1722939534_sabah-residence.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/96-sabah-residence-premium-yaay-kompleksi-amp-hotel.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Intercontinental Hotel BAKU",
     image: "/uploads/posts/2024-08/1722939442_intercont.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/95-intercontinental-hotel-baku.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Knights Bridge",
     image: "/uploads/posts/2024-08/1722939189_4569.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/94-knights-bridge.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Yavuz Sultan Səlim körpüsü",
     image: "/uploads/posts/2024-08/1722938994_yavuz-sultan-selim-koprusu-aa-1903882.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/93-yavuz-sultan-slim-korpusu.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Delta Telecom",
     image: "/uploads/posts/2024-08/1722938493_9787_delta4.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/92-delta-telecom.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Ravy Tower",
     image: "/uploads/posts/2024-08/1722934779_bakubayhorizontal-copy-min.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/91-ravy-tower.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Alov Qüllələri Residence",
     image: "/uploads/posts/2024-08/1722934390_alov-qulleleri.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/90-alov-qulllri-residence.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Şahdağ Turizm Mərkəzi",
     image: "/uploads/posts/2024-08/1722931129_sahdag.webp",
     url: "https://nareks.com/layiheler/bitmis-layiheler/89-ahda-turizm-mrkzi.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Azərbaycan Respublikası Montin Data Center Ağa Nemetulla Bakı",
     image: "/uploads/posts/2024-08/1722683369_istock-599903446-630x378.jpg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/85-azrbaycan-respublikas-montin-data-center-aa-nemetulla-bak.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Socar Azerkimya IB SUMGAYIT",
     image: "/uploads/posts/2024-08/1722674009_soc1.jpeg",
     url: "https://nareks.com/layiheler/bitmis-layiheler/79-socar-azerkimya-ib-sumgayit.html"
   },
   {
+    category: "Bitmiş layihələr",
     name: "Heydər Əliyev adına Neft Emalı Zavodu",
     image: "/uploads/posts/2024-08/1722673752_image-1-heydar-refinery.webp",
     url: "https://nareks.com/layiheler/bitmis-layiheler/77-heydr-liyev-adna-neft-emal-zavodu.html"
@@ -188,125 +224,170 @@ const bottomHeader = document.getElementById("bottomHeader");
 const headingProject = document.getElementById("heading-project")
 const navProject = document.getElementById("nav-project")
 const layiheler = document.querySelector(".projects__inner")
+const cardTitle = document.getElementById("card-title")
+const cardNav = document.getElementById("card-nav")
+const projectName = document.getElementById("project-name")
+const projectImg = document.getElementById("project-image")
+let project;
 
 const params = new URLSearchParams(window.location.search);
 const page = params.get("page");
+const id = Number(params.get("id"))
+console.log(params)
 
-if(page === 'ungoing' ) {
-    headingProject.innerHTML = "Davam edən layihələr"
-    navProject.innerHTML = "Ana səhifə  &rarr; Davam edən layihələr"
-    layiheler.innerHTML = davamEdenLayiheler.map(item=> (
-        `
+page == 'completed' ? project = bitmisLayiheler[id] : project = davamEdenLayiheler[id]
+
+
+if (page === 'ungoing') {
+  headingProject.innerHTML = "Davam edən layihələr"
+  navProject.innerHTML = "Ana səhifə  &rarr; Davam edən layihələr"
+  layiheler.innerHTML = davamEdenLayiheler.map((item, index) => (
+    `
             <div class="swiper-slide"> 
                 <span>${item.name}</span> 
                 <img src=${"https://nareks.com" + item.image}> 
-                <a href="https://nareks.com/layiheler/bitmis-layiheler/110-azrbaycan-prezident-administrasiyas.html" class="project-btn">Daha ətraflı</a> 
+                <a href=${"/card.html?id=" + index} class="project-btn">Daha ətraflı</a> 
             </div>
         `
-    )).join("");
-}else if(page === 'completed') {    
-    headingProject.innerHTML = "Bitmiş layihələr"  
-    navProject.innerHTML = "Ana səhifə  &rarr; Bitmiş layihələr"
-    layiheler.innerHTML = bitmisLayiheler.map(item=> (
-        `
+  )).join("");
+} else if (page === 'completed') {
+  if (headingProject) headingProject.innerHTML = "Bitmiş layihələr"
+  if (navProject) navProject.innerHTML = "Ana səhifə  &rarr; Bitmiş layihələr"
+  if (layiheler) layiheler.innerHTML = bitmisLayiheler.map((item, index) => (
+    `
             <div class="swiper-slide"> 
                 <span>${item.name}</span> 
                 <img src=${"https://nareks.com" + item.image}> 
-                <a href="https://nareks.com/layiheler/bitmis-layiheler/110-azrbaycan-prezident-administrasiyas.html" class="project-btn">Daha ətraflı</a> 
+                <a href=${"/card.html?page=completed&id=" + index} class="project-btn">Daha ətraflı</a> 
             </div>
         `
-    )).join("");  
+  )).join("");
+} else if(window.location.pathname === "/layiheler.html") {
+  if (headingProject) headingProject.innerHTML = "Layihələr"
+  if (navProject) navProject.innerHTML += "Layihələr"
+  if (layiheler) layiheler.innerHTML = `
+    <div class="sm:flex justify-between items-center w-full text-white font-bold whitespace-nowrap text-center">
+      <a href="/layiheler.html?page=completed" class="block px-4 hover:text-red-500">
+        Bitmiş layihələr
+      </a>
+      <a href="/layiheler.html?page=ungoing" class="block px-4 hover:text-red-500">
+        Davam edən layihələr
+      </a>
+    </div>
+  `
 }
 
-
 window.addEventListener("scroll", () => {
-    if (window.scrollY > 50) {
-        bottomHeader.classList.add("py-0");
-        bottomHeader.classList.remove("py-2");
-    } else {
-        bottomHeader.classList.remove("py-0");
-        bottomHeader.classList.add("py-2");
-    }
+  if (window.scrollY > 50) {
+    bottomHeader.classList.add("py-0");
+    bottomHeader.classList.remove("py-2");
+  } else {
+    bottomHeader.classList.remove("py-0");
+    bottomHeader.classList.add("py-2");
+  }
 });
 
 
+if (project) {
+
+  if (cardTitle) cardTitle.innerHTML = project.name;
+  if (cardNav) cardNav.innerHTML += project.category + ' <i class="fa-solid fa-right-long"></i> ' + project.name;
+  if (projectName) projectName.innerHTML = project.name;
+  if (projectImg) projectImg.src = "https://nareks.com" + project.image;
+
+} else {
+  console.error("Project tapılmadı!");
+}
+
 
 const swiper = new Swiper(".mySwiper", {
-    loop: true,
-    autoplay: {
-        delay: 3000,
-    },
-    speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  speed: 600,
 
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
 
 // Card Slider
 
 const swiperProjects = new Swiper(".swiper-projects", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    pagination: {
-        el: ".swiper-pagination-projects",
-        clickable: true, 
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination-projects",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    768: {
+      slidesPerView: 2,
     },
-    autoplay: {
-        delay: 3000, 
+    992: {
+      slidesPerView: 3,
     },
-    scrollbar: {
-        el: ".swiper-scrollbar",
-    },
-     breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        992: {
-            slidesPerView: 3,
-        },
-    }
+  }
 });
 
 const swiperReferences = new Swiper(".swiper-references", {
-    loop: true,
-    slidesPerView: 3,
-    spaceBetween: 20,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 3000,
+  },
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    autoplay: {
-        delay: 3000, 
+    768: {
+      slidesPerView: 2,
     },
-    scrollbar: {
-        el: ".swiper-scrollbar",
+    992: {
+      slidesPerView: 5,
     },
-     breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        768: {
-            slidesPerView: 2,
-        },
-        992: {
-            slidesPerView: 5,
-        },
-    }
+  }
 });
 
 
+
+const menuBtn = document.getElementById("menuBtn");
+const closeBtn = document.getElementById("closeBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("-translate-x-[320px]");
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("-translate-x-[320px]");
+});
